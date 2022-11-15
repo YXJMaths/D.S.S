13f08c93a6650238a492679030ca8e08b48dfabd
YXJMaths <116239379+YXJMaths@users.noreply.github.com>
2022-11-15 07:32:41 +0000

    modified:   C/main.c

--- C/main.c
+++ C/main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <math.h>
 #include "h/demo.h"
+//#include "h/for.h"
+//#include "h/while.h"
+//#include "h/if.h"
 int main(int argc, char *argv[]){
     demo();
     return 0;
