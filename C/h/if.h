3ec1b2c6ff42c005745c0765a4722ad25c1c9419
YXJMaths <116239379+YXJMaths@users.noreply.github.com>
2022-11-16 08:10:51 +0800

    更新 if.h

--- C/h/if.h
+++ C/h/if.h
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int cfor(){
-
+    char i;
+    printf("U yes Or No\n input y or n. \n");
+    scanf("%c",&i");
+    if(i = 'y' || i = 'Y'){
+        printf("Yes Agreed.\n");
+        }
+    else if(i = 'n' || i = 'N'){
+        printf("No Agreed.\n");
+        }
+    else{
+        printf("input error.\n");
+        }   
     return 0;
 }
